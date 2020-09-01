4d105309289418ed01f237428d4a6023586ff0c5
Karter13 <KarMiH1975@mail.ru>
2020-09-01 20:36:50 +0300

    created component Skill and styles for it

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import style from './Skills.module.css'
+import styleContainer from '../common/styles/Common.module.css'
+import {Skill} from './skill/Skill';
 
 export const Skills = () => {
     return (
-        <div className={style.skills}></div>
+        <div className={style.skillsBlock}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <h2 className={style.title}>SKILLS</h2>
+                <div className={style.skills}>
+                    <Skill title={'REACT'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                    <Skill title={'REDUX'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
+                    <Skill title={'HOC'} description={' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                </div>
+            </div>
+        </div>
     )
-}
+};
