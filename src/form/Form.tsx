a316a8c34a7bcd9db2ec72391a8ccc12d990c532
Karter13 <KarMiH1975@mail.ru>
2020-09-05 13:50:45 +0300

    created component Form and  styles module for it

---
+++ src/form/Form.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import style from './Form.module.css';
+import styleContainer from '../common/styles/Common.module.css';
+
+type FormPropsType = {}
+export const Form: React.FC<FormPropsType> = (props) => {
+    return (
+        <div className={style.formBlock}>
+            <div className={`${styleContainer.container} ${style.formContainer}`}>
+                <h2 className={style.title}>Contact</h2>
+                <form className={style.formBody}>
+                    <div className={style.name}>
+                        <input type="text" placeholder='name'/>
+                    </div>
+
+                    <div className={style.email}>
+                        <input type="email" placeholder='email'/>
+                    </div>
+                    <div className={style.text}>
+                        <textarea placeholder='text'/>
+                    </div>
+                </form>
+                <button className={style.button}>SEND MESSAGE</button>
+            </div>
+        </div>
+    )
+};
