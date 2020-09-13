d100aa6183f79cabc247075cec7ae3f60d174aba
Karter13 <KarMiH1975@mail.ru>
2020-09-13 10:52:58 +0300

    Updates

---
+++ precache-manifest.fe3aa9d1a2b60087e65e31431e7bab3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caeb9bd8f63605f8cb2af2264fcced30",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f67112afbd0393e377d7",
+    "url": "/portfolio/static/css/main.8156802e.chunk.css"
+  },
+  {
+    "revision": "391a6fb194d259d6ee98",
+    "url": "/portfolio/static/js/2.6b08fbae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.6b08fbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67112afbd0393e377d7",
+    "url": "/portfolio/static/js/main.42af9593.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
