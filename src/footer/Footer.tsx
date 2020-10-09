9e95dc1f07dc21f8726dcd2e8ef88d1bbcbd6b5e
Karter13 <KarMiH1975@mail.ru>
2020-10-09 20:27:53 +0300

    changed component Footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import style from './Footer.module.css'
-import {Networks} from './Neyworks/Networks';
 import styleContainer from '../common/styles/Common.module.css';
 
 type FooterPropsType = {}
 
 export const Footer: React.FC<FooterPropsType> = (props) => {
     return (
-        <div className={style.networksBlock}>
-            <div className={`${styleContainer.container} ${style.networksContainer}`}>
-                <h2>Maikl</h2>
-                <Networks/>
-                <span>2020</span>
+        <div className={style.footer}>
+            <div className={`${styleContainer.container} ${style.footerContainer}`}>
+                <h2 className={style.title}>Maikl</h2>
+                <div className={style.socialIcons}>
+                    <div className={style.socialIcon}></div>
+                    <div className={style.socialIcon}></div>
+                    <div className={style.socialIcon}></div>
+                    <div className={style.socialIcon}></div>
+                </div>
+                <span className={style.copyright}>2020</span>
             </div>
         </div>
     )
