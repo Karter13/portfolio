edadea44c861265d82608fe54d594d25225be1a2
Karter13 <KarMiH1975@mail.ru>
2020-10-13 12:03:43 +0300

    change css to scss for Main and added new styles

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import styleContainer from '../common/styles/Common.module.css'
-import style from './Main.module.css'
-
+import style from './Main.module.scss'
+import authorPhoto from '../assets/image/authorphoto.jpg'
 
 export const Main = () => {
+
+    const author = {
+        backgroundImage: `url('${authorPhoto}')`,
+    };
+
     return (
         <div className={style.mainBlock}>
             <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.text}>
-                    <span>Hi There</span>
-                    <h1>I am Jeffrey Aron</h1>
+                    <h1>Mikhail Karamzin</h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={style.photo}>
+                <div className={style.photo} style={author}>
                 </div>
             </div>
         </div>
