b64d6dc76d41b13eb08ece1c962c8f79fda08b7c
Karter13 <KarMiH1975@mail.ru>
2020-10-18 19:54:49 +0300

    fixed styles

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,23 +3,17 @@ import style from './Skills.module.scss'
 import styleContainer from '../common/styles/Common.module.css'
 import {Skill} from './skill/Skill';
 import {Title} from '../common/components/titile/Title';
-import jsIcon from '../assets/icons/js.jpg';
-import tsIcon from '../assets/icons/typescript.svg';
+import jsIcon from '../assets/icons/js.svg';
 
 export const Skills = () => {
-    const jS = {
-        backgroundImage: `url('${jsIcon}')`,
-    };
-    const tS = {
-        backgroundImage: `url('${tsIcon}')`,
-    };
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title title={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill style={jS} title={'JavaScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
-                    <Skill style={tS} title={'TypeScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
+                    <Skill style={jsIcon} title={'JavaScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
+                    <Skill title={'TypeScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
                     <Skill title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
                     <Skill title={'Redux'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
                     <Skill title={'Git'} description={' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,16 +1,17 @@
-import React, {CSSProperties} from 'react';
+import React from 'react';
 import style from './Skill.module.scss'
 
 type SkillPropsType = {
     title: string
     description: string
-    style?: CSSProperties
+    style?: string
 }
 
 export const Skill: React.FC<SkillPropsType> = (props) => {
     return (
         <div className={style.skill}>
-            <div className={style.iconBlock} style={props.style}>
+            <div className={style.iconBlock} >
+                <img className={style.logo} src = {props.style} alt='JS'/>
             </div>
             <h3 className={style.title}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
