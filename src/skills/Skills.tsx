7bb0d2ddb783bf26fb65c3cb80de1835dd27b996
Karter13 <KarMiH1975@mail.ru>
2020-11-15 22:21:48 +0300

    fixed project

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -6,15 +6,14 @@ import {Title} from '../common/components/titile/Title';
 import jsIcon from '../assets/icons/js.svg';
 
 export const Skills = () => {
-
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title title={'Skills'}/>
                 <div className={style.skills}>
-                    <Skill style={jsIcon} title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
-                    <Skill title={'TypeScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
-                    <Skill title={'Redux'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                    <Skill style={jsIcon} title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit'}/>
+                    <Skill title={'TypeScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit'}/>
+                    <Skill title={'Redux'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'}/>
                     <Skill title={'JavaScript'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '}/>
                     <Skill title={'Rest API'} description={' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
                     <Skill title={'Git'} description={' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
